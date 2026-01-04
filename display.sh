94cec7f3223f8f1d4185bf5fe2ab22f14268931a
Paul Zupan <paul.zupan.13@gmail.com>
2026-01-03 23:42:08 -0800

    Clean up on aisle root dir

--- display.sh
+++ display.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-window_width=1600
-window_height=900
+# 1280x720  720p
+
+window_width=1280
+window_height=720
 
 # These settings make the window big enough that we can see what the renderer
 # drew without squinting.
--- scripts/run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-cargo run && scripts/display.sh
